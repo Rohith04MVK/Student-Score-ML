2d6c72e01c66e2971b7a3ed73eae56c2662b4e50
Rohith04MVK <67266075+Rohith04MVK@users.noreply.github.com>
2020-10-05 19:18:22 +0530

    Improve accuracy using loop phase

--- studentScoreML.py
+++ studentScoreML.py
@@ -1,28 +1,44 @@
-import pandas as pd
 import numpy as np
-import sklearn
+import pandas as pd
 from sklearn import linear_model
+import sklearn
 from sklearn.utils import shuffle
+import pickle
+
 
 data = pd.read_csv("student-mat.csv", sep=";")
 
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
 
 predict = "G3"
-
+best = 0
 X = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
     X, y, test_size=0.1)
 
-linear = linear_model.LinearRegression()
 
-linear.fit(x_train, y_train)
+best = 0
+for _ in range(1000):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
+        X, y, test_size=0.1)
 
-acc = linear.score(x_train, y_train)
+    linear = linear_model.LinearRegression()
 
-predictions = linear.predict(x_test)  # Gets a list of all predictions
+    linear.fit(x_train, y_train)
+    acc = linear.score(x_test, y_test)
 
+    if acc > best:
+        best = acc
+        with open("studentgrades.pickle", "wb") as f:
+            pickle.dump(linear, f)
+
+
+pickle_in = open("studentgrades.pickle", "rb")
+linear = pickle.load(pickle_in)
+print(best)
+
+predictions = linear.predict(x_test)
 for x in range(len(predictions)):
-    print(f"The prediction: {predictions[x].round(0)} Actual: {y_test[x]}")
+    print(f"The prediction: {predictions[x].round(0)}   Actual: {y_test[x]}")
